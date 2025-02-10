3c43eaf6c7297a92939d062875f11b10b79f1c2c
al-furkan <furkanf4336@gmail.com>
2025-02-10 14:50:50 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Advocatenazmul</title>
+    <meta name="description" content>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/redoyimg/logo.png">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <!-- Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/loder.png" alt>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader Start -->
+
+    <header>
+        <!-- Header Start -->
+        <div class="header-area header-sticky">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <!-- Logo -->
+                    <div class="col-xl-2 col-lg-1 col-md-1">
+                        <div class="logo">
+                            <a href="index.html"><img src="assets/img/logo/logo.png" class="img-fluid"></a>
+                        </div>
+                    </div>
+                    <div class="col-xl-7 col-lg-8 col-md-6">
+                        <!-- Main-menu -->
+                        <div class="main-menu f-right d-none d-lg-block">
+                            <nav>
+                                <ul id="navigation">
+                                    <li><a href="index.php">Home</a></li>
+                                    <li><a href="./about.php">About</a></li>
+                                    <li><a href="./practice-area.php">Practice-Area</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
+                                    <li><a href="blog.html">Blog</a>
+                                        <ul class="submenu">
+                                            <li><a href="blog.php">Blog</a></li>
+                                            <li><a href="single-blog.php">Blog
+                                                    Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="../Case-ManageMent">Case-Management</a>
+                                        <ul class="submenu">
+                                            <li><a href="case_details.php">Case
+                                                    Details</a></li>
+                                            <li><a href="../Case-ManageMent/login.php">Case-Management Login</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-3">
+                        <!-- Header-btn -->
+                        <div class="header-btn d-none d-lg-block f-right">
+                            <a href="#" class="btn header-btn">Request
+                                Appointment</a>
+                        </div>
+                    </div>
+                    <!-- Mobile Menu -->
+                    <div class="col-12">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Header End -->
+    </header>
---
+++ practice-area.php
@@ -0,0 +1,43 @@
+<?php
+
+include('./header.php');
+
+?>
+<!-- Header End -->
+</header>
+
+<main>
+
+    <!-- slider Area Start-->
+    <div class="slider-area ">
+        <!-- Mobile Menu -->
+        <div class="single-slider slider-height2 d-flex align-items-center"
+            data-background="assets/img/hero/law_hero2.jpg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="hero-cap text-center">
+                            <h2>Services</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- slider Area End-->
+
+    <!-- Legal Practice Area Start-->
+    <?php
+
+include('./legal-practice-Area.php');
+
+?>
+
+    <!-- Legal Practice Area End-->
+</main>
+
+<?php
+
+include('./footer.php');
+
+?>
