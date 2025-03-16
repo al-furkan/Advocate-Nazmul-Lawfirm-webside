301110022c24d6117d9e43046e2526c647370142
al-furkan <furkanf4336@gmail.com>
2025-03-16 21:42:44 +0600

    aadad

--- blog.php
+++ blog.php
@@ -1,35 +1,23 @@
 <?php
 
 include('./header.php');
-
-?>
-
-<!-- slider Area Start-->
-<div class="slider-area ">
-    <!-- Mobile Menu -->
-    <div class="single-slider slider-height2 d-flex align-items-center" data-background="assets/img/hero/law_hero2.jpg">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="hero-cap text-center">
-                        <h2>Our Blog</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- slider Area End-->
-
-<!--================Blog Area =================-->
-<?php
 include('./db.php');
 
-// Fetch blog posts from the database
-$sql = "SELECT * FROM posts ORDER BY date DESC LIMIT 5"; // Adjust limit as per your needs
+// Pagination setup
+$limit = 5; // Posts per page
+$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+$offset = ($page - 1) * $limit;
+
+// Fetch blog posts with category names
+$sql = "SELECT posts.*, categories.name AS category_name 
+        FROM posts 
+        JOIN categories ON posts.category_id = categories.id 
+        ORDER BY date DESC 
+        LIMIT $limit OFFSET $offset";
 $result = $conn->query($sql);
 ?>
 
+<!-- Blog Area -->
 <section class="blog_area section-padding">
     <div class="container">
         <div class="row">
@@ -37,14 +25,15 @@ $result = $conn->query($sql);
                 <div class="blog_left_sidebar">
 
                     <?php
-                    // Loop through the blog posts
                     if ($result->num_rows > 0) {
-                        while($row = $result->fetch_assoc()) {
+                        while ($row = $result->fetch_assoc()) {
                     ?>
 
                     <article class="blog_item">
                         <div class="blog_item_img">
-                            <img class="card-img rounded-0" src="assets/img/blog/<?php echo $row['image']; ?>" alt="">
+                            <img class="card-img rounded-0"
+                                src="../Case-ManageMent/blog/uploads/<?php echo $row['image']; ?>"
+                                alt="../Case-ManageMent/blog/uploads/<?php echo $row['image']; ?>">
                             <a href="#" class="blog_item_date">
                                 <h3><?php echo date('d', strtotime($row['date'])); ?></h3>
                                 <p><?php echo date('M', strtotime($row['date'])); ?></p>
@@ -56,7 +45,7 @@ $result = $conn->query($sql);
                             </a>
                             <p><?php echo substr($row['content'], 0, 150); ?>...</p>
                             <ul class="blog-info-link">
-                                <li><a href="#"><i class="fa fa-user"></i> <?php echo $row['category']; ?></a></li>
+                                <li><a href="#"><i class="fa fa-user"></i> <?php echo $row['category_name']; ?></a></li>
                                 <li><a href="#"><i class="fa fa-comments"></i> <?php echo $row['comments']; ?>
                                         Comments</a></li>
                             </ul>
@@ -70,32 +59,53 @@ $result = $conn->query($sql);
                     }
                     ?>
 
+                    <!-- Pagination -->
                     <nav class="blog-pagination justify-content-center d-flex">
                         <ul class="pagination">
-                            <li class="page-item">
-                                <a href="#" class="page-link" aria-label="Previous">
-                                    <i class="ti-angle-left"></i>
-                                </a>
-                            </li>
-                            <li class="page-item">
-                                <a href="#" class="page-link">1</a>
-                            </li>
-                            <li class="page-item active">
-                                <a href="#" class="page-link">2</a>
-                            </li>
-                            <li class="page-item">
-                                <a href="#" class="page-link" aria-label="Next">
-                                    <i class="ti-angle-right"></i>
-                                </a>
-                            </li>
+                            <?php
+                            $total_posts_query = "SELECT COUNT(*) AS total FROM posts";
+                            $total_posts_result = $conn->query($total_posts_query);
+                            $total_posts = $total_posts_result->fetch_assoc()['total'];
+                            $total_pages = ceil($total_posts / $limit);
+
+                            for ($i = 1; $i <= $total_pages; $i++) {
+                                echo '<li class="page-item ' . ($page == $i ? "active" : "") . '">
+                                        <a href="index.php?page=' . $i . '" class="page-link">' . $i . '</a>
+                                      </li>';
+                            }
+                            ?>
                         </ul>
                     </nav>
                 </div>
             </div>
+
             <!-- Sidebar -->
             <div class="col-lg-4">
                 <div class="blog_right_sidebar">
-                    <!-- Your sidebar code here -->
+                    <aside class="single_sidebar_widget post_category_widget">
+                        <h4 class="widget_title">Categories</h4>
+                        <ul class="list cat-list">
+                            <?php
+                            // Fetch categories
+                            $category_sql = "SELECT categories.id, categories.name, COUNT(posts.id) AS post_count 
+                                             FROM categories 
+                                             LEFT JOIN posts ON categories.id = posts.category_id 
+                                             GROUP BY categories.id";
+                            $category_result = $conn->query($category_sql);
+
+                            if ($category_result->num_rows > 0) {
+                                while ($category = $category_result->fetch_assoc()) {
+                                    echo '<li>
+                                            <a href="index.php?category_id=' . $category['id'] . '" class="d-flex">
+                                                <p>' . $category['name'] . '</p>
+                                                <p>(' . $category['post_count'] . ')</p>
+                                            </a>
+                                          </li>';
+                                }
+                            }
+                            ?>
+                        </ul>
+                    </aside>
                 </div>
             </div>
         </div>
@@ -103,14 +113,6 @@ $result = $conn->query($sql);
 </section>
 
 <?php
-// Close the database connection
 $conn->close();
-?>
-
-<!--================Blog Area =================-->
-
-<?php
-
 include('./footer.php');
-
 ?>
